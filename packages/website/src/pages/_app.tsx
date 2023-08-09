700af469f06a21c595509a4d16548c3d365bc20d
Andrew Henry <andrew.henry@pitt.edu>
2023-08-08 21:21:34 -0400

    feat: add page analytics

--- packages/website/src/pages/_app.tsx
+++ packages/website/src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { Analytics } from '@vercel/analytics/react'
 import withDarkMode, { MODE } from 'next-dark-mode'
 import { AppProps } from 'next/app'
-
-import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
 import '@/styles/colors.css'
 import '@/styles/globals.css'
 
 function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default withDarkMode(MyApp, {
